9120a562b64962625803a17b9cf6ff33fcac0f86
Анастасия Ельшина <salnaya92@gmail.com>
2022-07-12 15:56:51 +0300

    Task1 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,38 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  'use strict';
+  
+  let arr = [];
+
+  let d = b**2 - 4*a*c;
+
+  
+  let x, x1;
+  
+  if (d < 0) {
+    console.log(arr);
+  } else if (d === 0) {
+      x = -b/(2*a);
+      arr.push(x);
+      console.log(arr);
+  
+  } else if (d > 0) {
+     x = (-b + Math.sqrt(d) )/(2*a);
+     x1 = (-b - Math.sqrt(d) )/(2*a);
+     arr.push(x, x1);
+     console.log(arr);
+    
+  }
+    return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+solveEquation(1, -4, 4);
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
+
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
