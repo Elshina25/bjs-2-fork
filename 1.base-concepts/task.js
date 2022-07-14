c4e806ea4cf9fdfe1234fd4b21cc2370027ec85a
Анастасия Ельшина <salnaya92@gmail.com>
2022-07-14 14:42:00 +0300

    "edited"

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,21 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  'use strict';
-  
   let arr = [];
-
   let d = b**2 - 4*a*c;
-
-  
   let x, x1;
   
   if (d < 0) {
-    console.log(arr);
+    arr;
+ 
   } else if (d === 0) {
       x = -b/(2*a);
       arr.push(x);
-      console.log(arr);
-  
+      
   } else if (d > 0) {
      x = (-b + Math.sqrt(d) )/(2*a);
      x1 = (-b - Math.sqrt(d) )/(2*a);
      arr.push(x, x1);
-     console.log(arr);
-    
   }
     return arr;
 }
@@ -30,49 +25,37 @@ function solveEquation(a, b, c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  'use strict';
+
 
   if (isNaN(percent)) {
     return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
-  } else {
-    percent = parseInt(percent);
   }
+    percent = parseInt(percent);
+  
 
   if (isNaN(contribution)) {
     return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
-  } else {
-    contribution = parseInt(contribution);
   }
+    contribution = parseInt(contribution);
+  
 
   if (isNaN(amount)) {
     return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
-  } else {
+  } 
     amount = parseInt(amount);
-  }
+  
 
 
   let creditBody = amount - contribution;
-
   let percent1 = percent / 100;
- 
   let percentRatio = percent1 / 12;
-
   let currentDate = new Date();
-
   let differenceInDate = date.getTime() - currentDate.getTime(); //разница между датами в миллисекундах
-  
   let month = 1000 * 60 * 60* 24 * 30; //месяц в миллисекундах 
-  
   let differenceInMonth = differenceInDate / month; //нахождение количества месяцев между датами 
   let creditPeriod = Math.trunc(differenceInMonth); 
-
   let mounthlyPayment = creditBody * (percentRatio + (percentRatio / (((1 + percentRatio) **creditPeriod) - 1)));
-
-
   let totalAmount = mounthlyPayment * creditPeriod;
-
-  
-
 
   return +totalAmount.toFixed(2);
 
