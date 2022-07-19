441d6f1d17265c44ab04744fa6da39bbd0f4adbf
Анастасия Ельшина <salnaya92@gmail.com>
2022-07-19 14:40:40 +0300

    Tasks is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,9 +6,10 @@ function compareArrays(arr1, arr2) {
 compareArrays([53, 45, 21],[53, 45, 22]);
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(items => items > 0).filter(items => items % 3 === 0).map(items => items * 10);
 
   // Ваш код
 
   return resultArr; // array
 }
+advancedFilter([3,64,-4,32,5,-98,7])
