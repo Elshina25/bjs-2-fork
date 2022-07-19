aea07cc8651b905a72c871a211239f8a67e4962d
Анастасия Ельшина <salnaya92@gmail.com>
2022-07-19 14:24:36 +0300

    Task1 is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  
-
-  return result; // boolean
+  // let result = JSON.stringify(arr1) === JSON.stringify(arr2);
+  let result = arr1.length === arr2.length && arr1.every((items, i) => i === arr2.indexOf(items));
+  return result;
 }
+compareArrays([53, 45, 21],[53, 45, 22]);
 
 function advancedFilter(arr) {
   let resultArr;
