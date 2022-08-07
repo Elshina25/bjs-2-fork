d11e7e4074fb66849758f2d8f39c10ad98ce5143
Анастасия Ельшина <salnaya92@gmail.com>
2022-08-07 20:21:43 +0300

    tasks is done

---
+++ 6.exception-closure/main.js
@@ -0,0 +1,7 @@
+const triangle = new Triangle(5, 3, 3);
+console.log(triangle.getPerimetr);
+console.log(triangle.getArea);
+
+const triangle1 = new Triangle(1, 1, 3);
+console.log(triangle1.getPerimetr);
+console.log(triangle1.getArea);
