7da8b1b477fd68c4095c4ea25597abf88d2af581
Анастасия Ельшина <salnaya92@gmail.com>
2022-08-07 20:22:12 +0300

    tasks is done again

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,55 @@
+//задача 1
+
+function parseCount(productCount) {
+    let parseNum = Number.parseInt(productCount);
+    if (isNaN(parseNum)) {
+        throw new Error('Невалидное значение');
+    } return parseNum;
+}
+parseCount(123);
+
+function validateCount(productCount) {
+    try {
+        return parseCount(productCount);
+    } catch(error) {
+        return error;
+    }
+}
+
+validateCount('56');
+
+//задача 2
+
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+
+        if (this.sideA + this.sideB < this.sideC || this.sideB + this.sideC < this.sideA || this.sideA + this.sideC < this.sideB) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    getPerimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    getArea() {
+        const semiPerimetr = 0.5*(this.sideA + this.sideB + this.sideC);
+        return parseFloat(Math.sqrt(semiPerimetr*(semiPerimetr - this.sideA)*(semiPerimetr - this.sideB)*(semiPerimetr - this.sideC)).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b , c);
+    } catch (error) {
+        return { getPerimeter: () => 'Ошибка! Треугольник не существует',
+            getArea: () => 'Ошибка! Треугольник не существует' };
+    }
+}
+
+getTriangle(5, 4, 6);
+getTriangle(1, 10, 3)
+
