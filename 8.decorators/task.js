8abcf9f999b1c8becc604c70d82072da6c48c1da
Анастасия Ельшина <salnaya92@gmail.com>
2022-09-06 12:11:58 +0300

    middle changes

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,22 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  return function(...args) {
+    const hash = args.join(',');
+    if (hash in cache) {
+      return cache[hash];
+    }
+
+    const result = func.call(cache, ...args);
+    cache[hash] = result;
+    return result;
+  } 
 }
+
+add2 = (a, b) => a + b;
+upgAdd2 = cachingDecoratorNew(add2);
+
+addThree = (a, b, c) => a + b + c;
+upgradedAddThree = cachingDecoratorNew(addThree);
 
 
 function debounceDecoratorNew(func) {
